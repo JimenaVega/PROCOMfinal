11a8da24ea6bb7ab01eb1e1beb1176e6b7e17a16
JimenaVega <jimevega28@gmail.com>
2020-07-22 19:42:44 -0300

    otros archivos necesarios para el vivado

---
+++ uP/fpga_files_procom.v
@@ -0,0 +1,12 @@
+/*-----------------------------------------------------------------------------
+-- Archivo       : fpga_files.v
+-- Organizacion  : Fundacion Fulgor 
+-- Fecha         : 2016-01-06
+-------------------------------------------------------------------------------
+-- Descripcion   : Incluye los paths a todos los archivos
+-------------------------------------------------------------------------------
+-- Autor         : Ariel Pola
+-------------------------------------------------------------------------------*/
+
+`include "C:\Users\miner\Documents\JIME\4toAnio\procom\vivado_stuff\rtl_procom\include\artyc_include.v"
+
