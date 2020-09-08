58a038cfcd623bfd64c48457a23775a9ccabc916
JimenaVega <jimevega28@gmail.com>
2020-09-08 14:27:20 -0300

    imagen standard + lineal para merge

--- python_resolutions/interfazWebUART.py
+++ python_resolutions/interfazWebUART.py
@@ -127,6 +127,27 @@ def fixedToFloat(NB,NBF,signedMode,num):
 		return (((num+2**(NB-1))&((2**NB)-1))-2**(NB-1))/(2**NBF)
 	elif(signedMode=='U'):
 		return num/(2**NBF)
+    
+def sendCol(imageCol,i):
+   
+    ser.write(imageCol)
+    time.sleep(0.001)
+    out = []
+    while (ser.inWaiting() > 0):
+        out.append(ser.read(1))
+        #print(out1)
+
+    #print('-'*7+"UART reading col #{}".format(i))
+    outInteger = []
+    for i in range(len(out)):
+       # print("sended = {0}  received = {1}".format(imageCol[i],int.from_bytes( out[i], "big")))
+        outInteger.append(int.from_bytes( out[i], "big"))
+    #print("amount of data received = {}".format(len(out)))
+    
+    ser.flushInput()
+    ser.flushOutput()
+    
+    return outInteger
 
 
     
@@ -151,6 +172,8 @@ print(ser.timeout)
 
 
 # In[1]: Main
+
+#----------------------lectura de imagen-------------------------------------------
 path = "descarga.jpg"
 #path = "foto1.jpg"
 
@@ -160,11 +183,11 @@ ap.add_argument("-k", "--kernel", help="Path to the kernel")
 args = ap.parse_args()
 
 #Load input image 
-image  = cv2.imread(args.image,1)
+image   = cv2.imread(args.image,1)
+endFlag = 0
 
 
-
-#Kernel to use
+#Kernel to use momentaneamente 
 kernel = np.array((
     	[-1, -1, -1],
     	[-1,  8, -1],
@@ -175,21 +198,24 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #cv2.imshow("Gray scale image", gray)
 [ROWIM,COLIM] = gray.shape
 print("filas gray = {0} \tcolumnas en gray = {1}".format(ROWIM,COLIM))
-#----Image normalization----
+#-----------------------Image normalization------------------------------------------------------------
 MaxIm     = 255.0
 MinIm     = 0.0
 newMaxIm  = 1.0
 newMinIm  = 0.0
     #--Linear--
 imageNormLin = linearNorm(gray, MaxIm, MinIm, newMaxIm, newMinIm)
+imageStandar = standarNorm(gray)
+
 
 #----Kernel normalization----
-maxKernel    = 8.0
-kernelL      = kernelNorm(kernel, maxKernel, 'linear')
+maxKernel  = 8.0
 
+kernelL    = kernelNorm(kernel, maxKernel, 'linear')
+kernelS    = kernelNorm(kernel, maxKernel, 'standar')
 #----Kernel flip----
 kernelLNorm  = flipKernel (kernelL)
-
+kernelSNorm  = flipKernel (kernelS)
 
 
 #------------------------------------------------------------------------------
@@ -199,8 +225,10 @@ kernelLNorm  = flipKernel (kernelL)
 
 # #kernel cuantizado con S(8,6)
 #imagen cuantizada con S(7,6)
-#test (3er elem) usado solo para poder ver los char unicode de espaciado
+
+quantImageSt= quantize(imageStandar,7,6)
 quantImage  = quantize(imageNormLin,7,6)
+
 quantKernel = quantize(kernelLNorm,8,6)
 
 
@@ -210,46 +238,44 @@ quantKernel = quantize(kernelLNorm,8,6)
 ser.flushInput()
 ser.flushOutput()
 
-def sendCol(imageCol,i):
-   
-    ser.write(imageCol)
-    time.sleep(0.001)
-    out = []
-    while (ser.inWaiting() > 0):
-        out.append(ser.read(1))
-        #print(out1)
+byteImage   = bytearray()
+byteImageStr = bytearray()
 
-    print('-'*7+"UART reading col #{}".format(i))
-    outInteger = []
-    for i in range(len(out)):
-       # print("sended = {0}  received = {1}".format(imageCol[i],int.from_bytes( out[i], "big")))
-        outInteger.append(int.from_bytes( out[i], "big"))
-    #print("amount of data received = {}".format(len(out)))
-    
-    ser.flushInput()
-    ser.flushOutput()
-    
-    return outInteger
+imRecons    = []
+imReconsStr = []
 
-byteImage = bytearray()
-
-imRecons = []
+#envio de imagen escalada linealmente
 for delta in range (COLIM):
     for i in range(ROWIM):
-        byteImage.append(quantImage[i+(delta*ROWIM)])
+        byteImage.append(quantImage[i+(delta*ROWIM)]) 
     imRecons.append(sendCol(byteImage,delta))
     byteImage.clear() 
-
+   
+#envio de imagen escalada con dev standard   
+for delta in range (COLIM):
+    for i in range(ROWIM):
+        byteImageStr.append(quantImageSt[i+(delta*ROWIM)])
+    imReconsStr.append(sendCol(byteImageStr,delta))
+    byteImageStr.clear()
 
 ser.close()
 
 for i in range(COLIM):
     for j in range(ROWIM):
-       imRecons[i][j]= fixedToFloat(7,6,'S',imRecons[i][j])
+       imRecons[i][j]    = fixedToFloat(7,6,'S',imRecons[i][j])
+       imReconsStr[i][j] = fixedToFloat(7,6,'S',imReconsStr[i][j])
 
-arrFloat = (np.asarray(imRecons,'float64').T) #imagen recontruida
+imageLinRec = (np.asarray(imRecons,'float64').T) #imagen reconstruida
+imageStrRec = (np.asarray(imReconsStr,'float64').T) #imagen recontruida
 
-#imagen original escalada imageNormLin
+endFlag = 1
+
+#------------------------------------------------------------------------------
+#---------------------------------output image--------------------------------         
+#------------------------------------------------------------------------------
+
+#imagen original escalada linealmente
+plt.figure(1)
 imMatrix, imHeight, imWidth   = conv2D(imageNormLin, kernelLNorm)
 maxVal, minVal                = search(imMatrix, imHeight, imWidth) 
 conv2DOutput                  = scale(imMatrix,maxVal, minVal, 'linear').astype('uint8')
@@ -257,11 +283,27 @@ conv2DOutput                  = scale(imMatrix,maxVal, minVal, 'linear').astype(
 plotHist(conv2DOutput,'original',1)
 cv2.imshow("1- original ", conv2DOutput)
 
-imMatrix, imHeight, imWidth   = conv2D(arrFloat, kernelLNorm)
+imMatrix, imHeight, imWidth   = conv2D(imageLinRec, kernelLNorm)
 maxVal, minVal                = search(imMatrix, imHeight, imWidth) 
 conv2DOutput                  = scale(imMatrix,maxVal, minVal, 'linear').astype('uint8')
 xConv,yConv                   = hist(conv2DOutput)
 plotHist(conv2DOutput,'reconst',2)
-cv2.imshow("2- reconstruida ", conv2DOutput)
+cv2.imshow("2- reconstruida lineal ", conv2DOutput)
 
 
+#imagen con escalado standard
+plt.figure(2)
+
+imMatrix, imHeight, imWidth   = conv2D(imageStandar, kernelLNorm)
+maxVal, minVal                = search(imMatrix, imHeight, imWidth) 
+conv2                         = scale(imMatrix,maxVal, minVal, 'linear').astype('uint8')
+#xConv,yConv                   = hist(conv2DOutput)
+plotHist(conv2DOutput,'original',1)
+#cv2.imshow("1- original ", conv2)
+
+imMatrix2, imHeight, imWidth  = conv2D(imageStrRec, kernelSNorm)
+maxVal, minVal                = search(imMatrix, imHeight, imWidth) 
+conv2DOutput2                 = scale(imMatrix2,maxVal, minVal,'standar').astype('uint8')
+xConv2,yConv2                 = hist(conv2DOutput2)
+plotHist(conv2DOutput,'reconstruida standard',2)
+cv2.imshow("2- reconstruida con std ", conv2DOutput2)
